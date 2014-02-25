3b4e053dbecc42afdf0186eae67e68658faee93a
Pedro Flores <pflores@dncp.gov.py>
2014-02-25 09:25:32 -0300

    Added basic edit form for Persona

--- app/controllers/PersonaController.java
+++ app/controllers/PersonaController.java
@@ -2,6 +2,8 @@ package controllers;
 
 import play.*;
 import play.mvc.*;
+import play.data.*;
+import static play.data.Form.*;
 
 import com.avaje.ebean.Page;
 
@@ -9,9 +11,15 @@ import java.util.List;
 
 import views.html.*;
 
+import play.i18n.Messages;
+
 import models.Persona;
 
 public class PersonaController extends Controller {
+
+  public static Result GO_HOME = redirect(
+      routes.PersonaController.list(0, 5, "nombre", "")
+  );
 
 	public static Result index() {
 		return ok(index.render());
@@ -22,12 +30,39 @@ public class PersonaController extends Controller {
 		return ok(views.html.persona.list.render(personas, sortBy));
 	}
 
+  public static Result create() {
+      Form<Persona> personaForm = form(Persona.class);
+      return ok(
+          views.html.persona.edit.render(null, personaForm)
+      );
+  }
+
 	public static Result edit(Long id){
-		return ok("Insercion de Personas");
+	  Form<Persona> computerForm = form(Persona.class).fill(
+	      Persona.find.byId(id)
+	  );
+	  return ok(
+	      views.html.persona.edit.render(id, computerForm)
+	  );
 	}
 
 	public static Result update(Long id){
-		return ok("Edicion de Personas");
+	  Form<Persona> personaForm = form(Persona.class).bindFromRequest();
+	  if(personaForm.hasErrors()) {
+	      return badRequest(views.html.persona.edit.render(id, personaForm));
+	  }
+	  if(id == null)
+			personaForm.get().save();
+	  else
+	  	personaForm.get().update(id);
+	  flash("success", Messages.get("persona_") + " has been updated");
+	  return GO_HOME;
 	}
+
+  public static Result delete(Long id) {
+      Persona.find.ref(id).delete();
+      flash("success", "Person has been deleted");
+      return GO_HOME;
+  }
 
 }
