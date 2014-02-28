09f143167543ac38a60e538d7704395406a804a8
Pedro Flores <pflores@dncp.gov.py>
2014-02-28 09:19:02 -0300

    Corrected some tabs and added more generic messages

--- app/controllers/PersonaController.java
+++ app/controllers/PersonaController.java
@@ -18,50 +18,50 @@ import models.Persona;
 public class PersonaController extends Controller {
 
   public static Result GO_HOME = redirect(
-      routes.PersonaController.list(0, 5, "nombre", "")
+    routes.PersonaController.list(0, 5, "nombre", "")
   );
 
-	public static Result index() {
-		return ok(index.render());
-	}
-
-	public static Result list(Integer page, Integer size, String sortBy, String filter){
-		Page<Persona> personas = Persona.page(page, size, sortBy, "asc", filter);
-		return ok(views.html.persona.list.render(personas, sortBy));
-	}
-
-  public static Result create() {
-      Form<Persona> personaForm = form(Persona.class);
-      return ok(
-          views.html.persona.edit.render(null, personaForm)
-      );
+  public static Result index() {
+    return ok(index.render());
   }
 
-	public static Result edit(Long id){
-	  Form<Persona> computerForm = form(Persona.class).fill(
-	      Persona.find.byId(id)
-	  );
-	  return ok(
-	      views.html.persona.edit.render(id, computerForm)
-	  );
-	}
+  public static Result list(Integer page, Integer size, String sortBy, String filter){
+    Page<Persona> personas = Persona.page(page, size, sortBy, "asc", filter);
+    return ok(views.html.persona.list.render(personas, sortBy));
+  }
 
-	public static Result update(Long id){
-	  Form<Persona> personaForm = form(Persona.class).bindFromRequest();
-	  if(personaForm.hasErrors()) {
-	      return badRequest(views.html.persona.edit.render(id, personaForm));
-	  }
-	  if(id == null)
-			personaForm.get().save();
-	  else
-	  	personaForm.get().update(id);
-	  flash("success", Messages.get("persona_") + " has been updated");
-	  return GO_HOME;
-	}
+  public static Result create() {
+    Form<Persona> personaForm = form(Persona.class);
+    return ok(
+      views.html.persona.edit.render(null, personaForm)
+    );
+  }
+
+  public static Result edit(Long id){
+    Form<Persona> computerForm = form(Persona.class).fill(
+      Persona.find.byId(id)
+    );
+    return ok(
+      views.html.persona.edit.render(id, computerForm)
+    );
+  }
+
+  public static Result update(Long id){
+    Form<Persona> personaForm = form(Persona.class).bindFromRequest();
+    if(personaForm.hasErrors()) {
+      return badRequest(views.html.persona.edit.render(id, personaForm));
+    }
+    if(id == null)
+      personaForm.get().save();
+    else
+      personaForm.get().update(id);
+    flash("success", Messages.get("persona_") + " " + Messages.get("msg_entity_updated"));
+    return GO_HOME;
+  }
 
   public static Result delete(Long id) {
       Persona.find.ref(id).delete();
-      flash("success", "Person has been deleted");
+      flash("success", Messages.get("persona_") + " " + Messages.get("msg_entity_updated"));
       return GO_HOME;
   }
 
